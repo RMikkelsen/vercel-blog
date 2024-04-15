f5bb1c72e6c3b2d0bb51b5098c3bc89219f4c95e
Ruth <rmm@impact.dk>
2024-04-15 14:35:11 +0200

    updated header and .env file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from "next/app";
+import { SessionProvider } from 'next-auth/react';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
+
+    // sessionProvider is from next-auth and wraps root component to persist user authenticaiton across entire application 
+    <SessionProvider session={pageProps.session}>
     <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
